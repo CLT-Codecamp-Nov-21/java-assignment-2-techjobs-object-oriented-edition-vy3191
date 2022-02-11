660ba18f9e88f24fae5183da3bf10e75f8f459ed
vy3191 <venkyafhs@gmail.com>
2022-02-11 11:32:48 -0500

    fixed some failing test specs

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -12,24 +12,18 @@ import static org.junit.Assert.*;
  */
 @RunWith(JUnit4.class)
 public class JobTest {
-    Job actualJob;
-    Job actualJob1;
-    @Before
-     public void createJobObject() {
-        actualJob = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        actualJob1 = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-    }
 
     @Test
     public void testSettingJobId() {
         Job myJob1 = new Job();
         Job myJob2 = new Job();
         assertNotEquals(myJob2.getId(), myJob1.getId());
-
     }
 
     @Test
     public void testJobConstructorSetsAllFields() {
+        Job actualJob = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
         assertNotNull(actualJob);
         assertEquals(actualJob.getName(), "product tester");
         assertEquals(actualJob.getEmployer().getValue(), "ACME");
@@ -47,12 +41,15 @@ public class JobTest {
 
     @Test
     public void testJobsForEquality() {
+        Job actualJob = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job actualJob1 = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertNotEquals(actualJob, actualJob1);
         assertFalse(actualJob.equals(actualJob1));
     }
 
     @Test
     public void testToStringStartsAndEndsWithNewLine() {
+        Job actualJob = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         String objString = actualJob.toString();
         char first = objString.charAt(0);
         char last =  objString.charAt(objString.length() - 1)  ;
@@ -64,6 +61,7 @@ public class JobTest {
 
     @Test
     public void testToStringContainsCorrectLabelsAndData() {
+        Job actualJob = new Job("product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         String objString = actualJob.toString();
         String expectedString = "\n" +
                 "ID: " +  actualJob.getId() + "\n"+
